50c8c719629669f7bd65df89e6c82596e756db59
VFM | SB <vfm_sb@proton.me>
2023-04-03 22:55:56 +0300

    bagels-v2 -- Initial Project Commit -- Copy of bagels-generic -- Additional Instructions & Modified generate_secret_number()

---
+++ bagels-v2/bagels.py
@@ -0,0 +1,99 @@
+"""
+Bagels -- Version 2
+A Deductive Number Guessing Game.
+Description:
+    The secret number will be any length between 2 and 10 digits.
+    Player must guess a unique-digits number based on clues.
+    Player will have 10 attempts to guess the random number.
+"""
+
+# Built-in Modules
+import os
+import random
+import string
+
+
+# Functions
+def generate_secret_number() -> str:
+    number_of_digits = random.randint(2, 10)
+    return "".join(random.sample(string.digits, number_of_digits))
+
+
+def valid_guess(guess: str) -> bool:
+    if not guess.isdigit():
+        return False
+    if len(guess) != 3:
+        return False
+    for digit in guess:
+        if guess.count(digit) > 1:
+            return False
+    return True
+
+
+def pico_fermi_bagels(secret_number: str, guess: str) -> list[str]:
+    hints = []
+    for index, digit in enumerate(guess):
+        if digit == secret_number[index]:
+            hints.append("Pico")
+            continue
+        if digit in list(secret_number):
+            hints.append("Fermi")
+    if len(hints) == 0:
+        hints.append("Bagels")
+    return hints
+
+
+def bagels_game() -> None:
+    header()
+    secret_number = generate_secret_number()
+    print("Random Number Has Been Determined! Hint: {}".format(secret_number))
+    print("You Have 10 Guesses to Find The Number.")
+    guess_count = 1
+    while guess_count <= 10:
+        print()
+        print(f"Guess #{guess_count}")
+        guess = input("> ")
+        if len(guess) == 2:
+            guess = "0" + guess
+        if not valid_guess(guess):
+            print("Not a Valid Guess\nGuess Again!")
+            continue
+        guess_count += 1
+        hints = pico_fermi_bagels(secret_number, guess)
+        if secret_number == guess:
+            print("You Got It!")
+            return
+        print(" ".join(sorted(hints)))
+    print("You Ran Out of Guesses!")
+    print(f"The Number was {secret_number}")
+
+
+def header():
+    print(
+        "Bagels, A Deductive Logic Game\n"
+        f"{'-' * len('Bagels, A Deductive Logic Game')}\n"
+        "Instructions:\n"
+        "Pico:     If One Digit is Correct but in the Wrong Position.\n"
+        "Fermi:    If One Digit is Correct and in the Right Position.\n"
+        "Bagels:   If No Digits is Correct.\n"
+    )
+
+
+def main() -> None:
+    while True:
+        bagels_game()
+        print("\nWould You Like To Play Again? (yes or no)")
+        answer = input("> ").lower()
+        if answer == "yes":
+            if os.name == "posix":
+                os.system("clear")
+            elif os.name == "nt":
+                os.system("cls")
+            continue
+        break
+    print("Thanks For Playing!")
+
+
+# Run The Game
+if __name__ == "__main__":
+    main()
---
+++ bagels-v2/instructions.md
@@ -0,0 +1,9 @@
+# Bagels V2 Instructions
+
+In this version of Bagels, the secret number can be any length between 2 and 10 digits. Additionally, you will have a limited number of guesses, which will decrease by one each time you make a guess. You will also be allowed to make several different types of guesses:
+
+-   A normal guess, where you guess a number and get the standard “Pico”, “Fermi”, or “Bagels” hint.
+
+-   A “Hint” guess, where you ask for a hint instead of making a guess. You will receive one hint about the secret number for each hint guess you make. However, each hint guess will also decrease your remaining number of guesses by one.
+
+-   A “Force” guess, where you force the game to tell you the correct answer. You will lose the game immediately if you make a force guess.
