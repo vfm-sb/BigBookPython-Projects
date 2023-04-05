e16a4f77ec22a63656ea0de26039965cd09af7dc
VFM | SB <vfm_sb@proton.me>
2023-04-05 23:12:30 +0300

    birthday_paradox -- Added generate_birthdays() Func

--- birthday_paradox/birthday_paradox.py
+++ birthday_paradox/birthday_paradox.py
@@ -7,8 +7,12 @@ import calendar
 import random
 
 
-def random_birthday():
+def random_birthday() -> datetime.date:
     year = datetime.date.today().year
     total_days = 365 if calendar.isleap(year) else 364
     nth_day = random.randint(0, total_days)
     return datetime.date(year, 1, 1) + datetime.timedelta(days=nth_day)
+
+
+def generate_birthdays(n: int) -> list[datetime.date]:
+    return [random_birthday() for _ in range(n)]
