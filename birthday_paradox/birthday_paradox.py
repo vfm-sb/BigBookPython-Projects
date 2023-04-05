274d207c1e20443a2080ab712f03be548e1db841
VFM | SB <vfm_sb@proton.me>
2023-04-05 22:33:23 +0300

    birthday_paradox -- Added random_birthday() Func

---
+++ birthday_paradox/birthday_paradox.py
@@ -0,0 +1,14 @@
+"""
+Birthday Paradox Simulation
+"""
+
+import datetime
+import calendar
+import random
+
+
+def random_birthday():
+    year = datetime.date.today().year
+    total_days = 365 if calendar.isleap(year) else 364
+    nth_day = random.randint(0, total_days)
+    return datetime.date(year, 1, 1) + datetime.timedelta(days=nth_day)
